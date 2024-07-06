954dc994512046b3ed074ef0dcba14976b0136cc
rakshithvk19 <rakshithvk19@gmail.com>
2024-07-06 10:00:13 -0400

    Initial commit

---
+++ src/HelloWorld.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./interfaces/IERC20.sol";
+
+contract HelloWorld {
+    /**
+     *  HELLO WORLD EXERCISE
+     *
+     *  The contract returns Hello World string.
+     *
+     */
+    function sayHelloWorld(address token) public view returns (string memory) {
+        return IERC20(token).name();
+    }
+}
